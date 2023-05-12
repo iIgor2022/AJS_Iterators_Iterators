a1bd6efd71ec9d0fc66e5850d3a75010ca289dcf
Овчинников Игорь <faceplaces@inbox.ru>
2023-05-12 11:01:17 +0300

    first commit

---
+++ src/__tests__/app.js
@@ -0,0 +1,17 @@
+import Team from '../app';
+
+test('Testing Team iterator', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+  const iterator = new Team(character, character, character);
+  /* eslint-disable-next-line */
+  for (const item of iterator) {
+    expect(item).toEqual(character);
+  }
+});
---
+++ src/app.js
@@ -0,0 +1,25 @@
+export default class Team {
+  constructor(...characters) {
+    this.characters = characters;
+  }
+
+  [Symbol.iterator]() {
+    let index = 0;
+    const characters = this.characters;
+    return {
+      next() {
+        index++;
+        if (index > characters.length) {
+          return {
+            value: undefined,
+            done: true,
+          };
+        }
+        return {
+          value: characters[index - 1],
+          done: false,
+        };
+      },
+    };
+  }
+}
